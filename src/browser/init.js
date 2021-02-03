b2478ac261086ee247d76ee5496d588e49daec9f
Solaiman  Kmail <psokmail@gmail.com>
2021-02-03 23:02:35 +0200

    fix: iframe ads, remove html room attributes on extension initialize

--- src/browser/init.js
+++ src/browser/init.js
@@ -8,13 +8,31 @@
   removeChildren(document.head)
   removeChildren(document.body)
 
-  Object.keys(document.body.attributes).forEach(attribute => {
-    document.body.removeAttribute(attribute)
-  })
+  const removeAttributes = element => {
+    Object.values(element.attributes).forEach(attribute => {
+      element.removeAttribute(attribute.name)
+    })
+  }
+
+  removeAttributes(document.body)
+  removeAttributes(document.documentElement)
 
   const appRoot = document.createElement('div')
 
   appRoot.id = 'RESPONSIVE-VIEWER-ROOT'
 
   document.body.appendChild(appRoot)
+
+  window.addEventListener('message', function(message) {
+    if (message.data.message !== '@ADS/LOADED') {
+      return
+    }
+
+    const iframe = document.getElementById('advertismentIframe')
+
+    const height = Math.min(150, message.data.height)
+
+    iframe.style.width = '100%'
+    iframe.style.height = `${height}px`
+  })
 })()
--- src/components/Advertisement/index.js
+++ src/components/Advertisement/index.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import './style.css'
 const Advertisement = props => {
-  const container = React.useRef()
-
-  React.useEffect(() => {
-    const id = '_carbonads_js'
-    const serve = 'CE7DK5QY'
-    const placement = 'githubcom'
-    const src = `https://cdn.carbonads.com/carbon.js?serve=${serve}&placement=${placement}`
-    const script = document.createElement('script')
-    script.id = id
-
-    container.current.appendChild(script)
-    script.src = src
-  }, [])
-
-  return <div ref={container} />
+  return (
+    <iframe
+      title="Advertisment"
+      src="https://responsive-viewer-ads.netlify.app/"
+      scrolling="no"
+      frameBorder="no"
+      id="advertismentIframe"
+    ></iframe>
+  )
 }
 
 export default Advertisement
